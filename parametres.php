60d018529fed45b026ad0099fbdd6b9f39561bc5
olias72 <nov72ol@gmail.com>
2015-02-26 22:15:42 +0100

    XML data OK

--- parametres.php
+++ parametres.php
@@ -1,13 +1,13 @@
 <?php
-	phpinfo();
-	/*$dom = new DomDocument();
-	$dom->load('moneyDrop.xml');
-	$miseDeDepart = $dom->getElementsByTagName('miseDeDepart');
-	$listeMontant = $miseDeDepart->firstChild->nodeValue;
-	//foreach ($listeMontant as $m) {
-	//	echo $m->firstChild->nodeValue . "<br>";
-	//}
-	echo $miseDeDepart;
-	echo $listeMontant;*/
-	//echo 'test';
+	if (file_exists('moneyDrop.xml')) {
+    $xml = simplexml_load_file('moneyDrop.xml');
+    //print_r($xml);
+    
+    foreach ($xml->miseDeDepart->montant as $mise) {
+		echo $mise . "<br>";
+		}
+	} 
+	else {
+	    exit('Echec lors de l\'ouverture du fichier test.xml.');
+	}
 ?>
